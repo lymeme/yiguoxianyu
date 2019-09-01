165ff156e468b6834c1523038550c521d8bee3f2
梁梓政 <liangzizheng@126.com>
2019-09-01 21:38:01 +0800

    01-项目结构初始化

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <el-carousel :interval="5000" arrow="never">
+      <el-carousel-item v-for="(item,index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 轮播图数据
+      banners: [
+        {
+          url: "http://157.122.54.189:9095/assets/images/th03.jfif"
+        },
+        {
+          url: "http://157.122.54.189:9095/assets/images/th04.jfif"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+
+  /deep/ .el-carousel__container {
+    height: 565px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
